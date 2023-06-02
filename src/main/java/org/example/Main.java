9dc9436182bb09d98376137c005751276a4b1156
Lima <lukinha-lenta@hotmail.com>
2023-06-02 11:01:33 -0300

    Insert Input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.apache.poi.ss.usermodel.*;
 
+import javax.swing.*;
 import java.io.FileInputStream;
 import java.sql.*;
 import java.util.ArrayList;
@@ -11,11 +12,21 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "caminho\\planilha.xlsx";
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
+        String filePath = null;
+        if (result == JFileChooser.APPROVE_OPTION) {
+            filePath = fileChooser.getSelectedFile().getAbsolutePath();
+            JOptionPane.showMessageDialog(null,"Arquivo Selecionado: " + filePath);
+        } else if (result == JFileChooser.CANCEL_OPTION) {
+            JOptionPane.showMessageDialog(null,"Seleção de arquivo cancelada");
+        } else if (result == JFileChooser.ERROR_OPTION) {
+            JOptionPane.showMessageDialog(null,"Erro ao selecionar arquivo");
+        }
+        String username = JOptionPane.showInputDialog("Usuario do DB");
+        String password = JOptionPane.showInputDialog("Senha do DB");
         String tableName = "product";
-        String url = "jdbc:mysql://localhost:3306/banco";
-        String username = "root";
-        String password = "senha";
+        String url = "jdbc:mysql://localhost:3306/db000";
         String defaultValue = "";
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             FileInputStream fileInputStream = new FileInputStream(filePath);
@@ -62,7 +73,7 @@ public class Main {
                 Cell priceCell = row.getCell(3);
                 Cell currentStockCell = row.getCell(4);
 
-                //Leitura de nome repetido
+               //Leitura de nome repetido
                 String name = dataFormatter.formatCellValue(nameCell);
                 if (nomesLidos.contains(name)) {
                     continue;
@@ -101,6 +112,7 @@ public class Main {
                     //Update do internal_code
                     preparedStatement.addBatch("UPDATE " + tableName + " SET internal_code = id");
                     preparedStatement.addBatch("UPDATE " + tableName + " SET description = ''");
+                    preparedStatement.addBatch("UPDATE " + tableName + " SET type = 1");
                     preparedStatement.addBatch("UPDATE " + tableName + " SET category_id = 2");
                     preparedStatement.addBatch("UPDATE " + tableName + " SET department_id = 1");
                     preparedStatement.addBatch("UPDATE " + tableName + " SET measure_unit = 'u'");
